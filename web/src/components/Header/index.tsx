9ac9cca92fe19fbcc5ba9de11308b2ef21990ab9
Alerrando <alerrando2@gmail.com>
2023-12-10 01:02:40 -0300

    feat: add transition login and register

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -4,7 +4,9 @@ export function Header(){
     return(
         <header className="w-full h-auto bg-white fixed top-0 border-b border-b-[#00938c]">
             <div className="w-11/12 h-full flex items-center justify-between mx-auto">
-                <img src="/public/ultralinks-logo.svg" alt="" className="h-16 w-44 object-fill" />
+                <Link to="/" className="h-16 w-44">
+                    <img src="/public/ultralinks-logo.svg" alt="" className="w-full h-full object-fill" />
+                </Link>
 
                 <div className="w-auto h-full flex flex-row items-center justify-center gap-4 py-2">
                     <Link to="/login" className="hover:bg-[#00938c] border border-transparent hover:border-[#00938c] hover:text-white px-4 py-1 rounded-lg cursor-pointer transition-all">
